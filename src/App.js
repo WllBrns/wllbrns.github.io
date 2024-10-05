4176f68c8a4aef506a851edebfeeea58c3f5b712
William Burns <54171303+WllBrns@users.noreply.github.com>
2024-10-05 14:52:49 +1000

    Added label to button

--- src/App.js
+++ src/App.js
@@ -30,10 +30,12 @@ function App() {
           name="First Project" 
           img="../public/placeholder.jpeg" 
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        >
-
-        </ProjectArticle>
-        <ProjectArticle name="Second Project" img="../public/placeholder.jpeg" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."></ProjectArticle>
+        />
+        <ProjectArticle 
+          name="Second Project" 
+          img="../public/placeholder.jpeg" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        />
       </article>
 
     </div>
--- src/components/ProjectArticle/ProjectArticle.js
+++ src/components/ProjectArticle/ProjectArticle.js
@@ -3,9 +3,9 @@ import { Button } from '../Button/Button';
 
 export const ProjectArticle = ({name, description, img}) => {
     return <article className='projectArticle'>
-        <img className='projectImg' src={img}></img>
+        <img className='projectImg' src={img}/>
         <h2>{name}</h2>
         <p>{description}</p>
-        <Button></Button>
+        <Button label={`Go to ${name}`}></Button>
     </article>
 }
