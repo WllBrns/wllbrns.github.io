fd25242c0ccdf84881b33728db2b85ab200199bd
William Burns <54171303+WllBrns@users.noreply.github.com>
2024-10-05 15:06:41 +1000

    formatting

--- src/components/ProjectArticle/ProjectArticle.js
+++ src/components/ProjectArticle/ProjectArticle.js
@@ -1,11 +1,13 @@
-import './ProjectArticle.css';
-import { Button } from '../Button/Button';
+import "./ProjectArticle.css";
+import { Button } from "../Button/Button";
 
-export const ProjectArticle = ({name, description, img}) => {
-    return <article className='projectArticle'>
-        <img className='projectImg' src={img}/>
-        <h2>{name}</h2>
-        <p>{description}</p>
-        <Button label={`Go to ${name}`}></Button>
+export const ProjectArticle = ({ name, description, img }) => {
+  return (
+    <article className="projectArticle">
+      <img className="projectImg" src={img} />
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <Button label={`Go to ${name}`}></Button>
     </article>
-}
+  );
+};
